4d10c929216bdc6beda20ad748169dbb36040c97
Samrat Mukherjee <samratmukherjee.sps@gmail.com>
2020-02-11 08:48:02 +0530

    made the API calls

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -9,25 +9,52 @@ class Loading extends StatefulWidget {
 
 class _LoadingState extends State<Loading> {
 
-  void getData() async {
+  void getTime() async {
 
-    Response response = await get('https://jsonplaceholder.typicode.com/todos/1');
+    /*This is example of calling an API from a website*/
+    // Response response = await get('https://jsonplaceholder.typicode.com/todos/1');
+    // Map data = jsonDecode(response.body);
+    // print(data);
+    // print(data['title']);
+
+    /*Calling the world time API*/
+
+    //making the request
+    Response response = await get('http://worldtimeapi.org/api/timezone/Asia/Kolkata');
     Map data = jsonDecode(response.body);
-    print(data);
-    print(data['title']);
+    
+    //get properties from the data
+    String datetime = data['datetime'];
+    String offset = data['utc_offset'];
+
+    String offset_hour = offset.substring(1, 3);
+    String offset_min = offset.substring(4, 6);
+
+    // print(datetime);
+    print(offset);
+
+    //create a datetime object
+    DateTime now = DateTime.parse(datetime);
+    now = now.add(Duration(hours: int.parse(offset_hour), minutes: int.parse(offset_min)));
+    print(now);
 
   }
 
   @override
   void initState() {    
     super.initState();
-    getData();
+    getTime();
     
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(      
+    return Scaffold(    
+      appBar: AppBar(
+        title: Text('Loading screen'),
+        backgroundColor: Colors.blueAccent,
+        centerTitle: true,
+      ),  
       body: SafeArea(
         child: Text('Loading screen')
       ),
